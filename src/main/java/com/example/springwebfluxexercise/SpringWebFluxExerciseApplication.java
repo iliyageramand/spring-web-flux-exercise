8a3b0b6bd07ebeecd098ff9e257502532530898d
Iliya Geramand <iliyageramand@gmail.com>
2023-08-08 15:22:56 +0330

    project initialization

---
+++ src/main/java/com/example/springwebfluxexercise/SpringWebFluxExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebfluxexercise/SpringWebFluxExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
