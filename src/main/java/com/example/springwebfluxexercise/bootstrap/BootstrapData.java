886490175202dcb97bbb1465f41fc3504a4c8295
Iliya Geramand <iliyageramand@gmail.com>
2023-08-12 20:03:56 +0330

    check for existing data before inserting initial data

--- src/main/java/com/example/springwebfluxexercise/bootstrap/BootstrapData.java
+++ src/main/java/com/example/springwebfluxexercise/bootstrap/BootstrapData.java
@@ -20,10 +20,25 @@ public class BootstrapData implements CommandLineRunner {
     private final PersonRepository personRepository;
     private final CourseRepository courseRepository;
     private final PersonCourseRepository personCourseRepository;
+    private final String[] titles = {"AP", "DS", "Math", "Physics", "AI", "ML", "Network", "security", "web", "SE"};
 
     @Override
     public void run(String... args) throws Exception {
-        String[] titles = {"AP", "DS", "Math", "Physics", "AI", "ML", "Network", "security", "web", "SE"};
+        Long peopleCount = personRepository.count().block();
+        if (peopleCount != null && peopleCount == 0) {
+            loadPeopleData();
+        }
+        Long coursesCount = courseRepository.count().block();
+        if (coursesCount != null && coursesCount == 0) {
+            loadCoursesData();
+        }
+        Long peopleCoursesCount = personCourseRepository.count().block();
+        if (peopleCoursesCount != null && peopleCoursesCount == 0) {
+            loadPeopleCourses();
+        }
+    }
+
+    private void loadPeopleData() {
         for (int i = 0; i < 10; i++) {
             long min = LocalDate.of(1980, Month.JANUARY, 1).toEpochDay();
             long max = LocalDate.of(2005, Month.DECEMBER, 31).toEpochDay();
@@ -35,13 +50,21 @@ public class BootstrapData implements CommandLineRunner {
                     .lastName("lastName" + (i + 1))
                     .birthdate(birthdate)
                     .build();
+            personRepository.save(person).subscribe();
+        }
+    }
+
+    private void loadCoursesData() {
+        for (int i = 0; i < 10; i++) {
             Course course = Course.builder()
                     .instructorId((long) (i + 1))
                     .title(titles[i])
                     .build();
-            personRepository.save(person).subscribe();
             courseRepository.save(course).subscribe();
         }
+    }
+
+    private void loadPeopleCourses() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 if (i == j)
